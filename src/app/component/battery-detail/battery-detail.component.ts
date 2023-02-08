b88a6b7abdd1515e01f0b144864b80db6f6d5908
성민 <tjdals2529@naver.com>
2023-02-08 14:23:35 +0900

    모니터링 배터립 팝업 진행중

--- src/app/component/battery-detail/battery-detail.component.ts
+++ src/app/component/battery-detail/battery-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import * as echarts from 'echarts';
 
 @Component({
   selector: 'app-battery-detail',
@@ -15,10 +16,42 @@ export class BatteryDetailComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.setVoltageChart()
+    }, 100);
   }
 
   close(){
     this.dialogRef.close()
+  }
+
+
+
+
+  setVoltageChart(){
+    var chartDom = document.getElementById('voltageChart')!;
+    var myChart = echarts.init(chartDom);
+    var option: echarts.EChartsOption;
+
+    option = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+//        show :false
+
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [120, 200, 150, 80, 70, 110, 130],
+          type: 'bar'
+        }
+      ]
+    };
+
+    option && myChart.setOption(option);
   }
 
 
