f09bfc286767292df1316938dfbe5e32978a1288
Nchumpeni9720 <achumshitiri@gmail.com>
2023-05-30 16:03:39 +0900

    중국어 번역 추가

--- src/app/layout/monitoring/monitoring.component.ts
+++ src/app/layout/monitoring/monitoring.component.ts
@@ -8,19 +8,29 @@ import { RealtimedataService } from 'src/app/service/realtimedata.service';
 import { UiService } from 'src/app/service/ui.service';
 import { UtilService } from 'src/app/service/util.service';
 
+import { TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-monitoring',
   templateUrl: './monitoring.component.html',
   styleUrls: ['./monitoring.component.css']
 })
 export class MonitoringComponent implements OnInit {
+  selectedLanguage: string; // Property to track the selected language(MINE)
+  translationFile : string = ""
+  // stateOptions: { label: string; value: string; }[];
 
   @ViewChild('realTimeMonitoringGrid', { read: ElementRef }) realTimeMonitoringGrid : ElementRef;
 
   constructor(   private router: Router,
     private realtimedataService : RealtimedataService,
     private uiService : UiService,
-    private utilService : UtilService) { }
+    private utilService : UtilService,
+
+    private translate: TranslateService,
+    private http: HttpClient
+    ) { }
 
   startDate : any
   endDate :any
@@ -28,7 +38,7 @@ export class MonitoringComponent implements OnInit {
   mapsBtn$ : Subscription
 
   columnDefs: ColDef[] = [
-    { headerName: 'Login', width:80, cellRenderer : LoginCarRendererComponent},
+    { headerName: this.translate.instant('translationKeyLogin'), width:80, cellRenderer : LoginCarRendererComponent},
     { field: 'vin', headerName: 'VIN', tooltipField: 'vin', width:180},
     { field: 'regNumber', headerName : 'Reg. number', tooltipField: 'regNumber', width:130},
     { field: 'nemsSn', headerName : 'NEMS S/N', tooltipField: 'nemsSn', width:180},
@@ -55,19 +65,98 @@ export class MonitoringComponent implements OnInit {
   gridHeight : number
   pageSize : number
   currentPage : number = 1
-
   ngAfterViewInit() {
     this.getPageSize()
   }
 
   ngOnInit(): void {
+    this.selectedLanguage = 'en'; // Set the default language
+    this.translate.setDefaultLang('en'); // Set the default language
+  
+    // Load the translation file for the selected language
+    this.translationFile = `../assets/i18n/dashboard/${this.selectedLanguage}.json`;
+
+    this.uiService.currentLanguage$.subscribe((language : string)=>{
+      console.log(language)
+      this.selectedLanguage = language
+      this.translationFile = `../assets/i18n/dashboard/${this.selectedLanguage}.json`;
+
+      if(this.selectedLanguage == 'en'){
+        console.log("영어")
+      }else {
+        console.log("중문")
+      }
+      this.translateColumnHeaders()
+    })
+
     this.startDate = new Date(new Date().getTime() -1*1000*60*60*24);
     this.endDate = new Date(new Date().getTime());
     this.page$ = this.uiService.page$.subscribe((page : number)=>{
       this.currentPage = page
       this.getRealtimedataVehiclelist()
     })
-  }
+   }
+
+   translateColumnHeaders(): void {
+
+    this.translate.use(this.selectedLanguage).subscribe(() => {
+      this.http.get<any>(this.translationFile).subscribe((data) => {
+        this.translate.setTranslation(this.selectedLanguage, data);
+        console.log('Translation file loaded successfully');
+        this.translate.get([ 'VIN', 'Reg. number', 'NEMS S/N', 'Last Updated', 'Accumulated Mile(km)', 'Packet Count', 'model', 'region', 'Purpose', 'Warning', 'soc(%)' ]).subscribe((translations: any) => {
+          console.log(translations['vin']); // Check the translation value
+          console.log(translations['regNumber']);
+          console.log('Language:', this.translate.currentLang); // Log the current language
+          console.log('Translations:', translations); // Log the translations object
+          this.columnDefs = [
+            { headerName: 'Login', width:80, cellRenderer : LoginCarRendererComponent},
+            { field: 'vin', headerName: translations['VIN'], tooltipField: 'vin', width:180},
+            { field: 'regNumber', headerName : translations['Reg. number'], tooltipField: 'regNumber', width:130},
+            { field: 'nemsSn', headerName : translations['NEMS S/N'], tooltipField: 'nemsSn', width:180},
+            { field: 'lastUpdate', headerName : translations['Last Updated'], valueFormatter : this.utilService.gridDateFormat, tooltipField: 'lastUpdate'},
+            { field: 'accumulatedMile', headerName : translations[ 'Accumulated Mile(km)'], tooltipField: 'accumulatedMile', width:200 },
+            { field: 'packetCount', headerName : translations['Packet Count'], tooltipField: 'packetCount', width:150 },
+            { field: 'model', headerName : translations['model'], tooltipField: 'model', width:100 },
+            { field: 'region', headerName : translations['region'], tooltipField: 'region', width:100 },
+            { field: 'purpose', headerName : translations['Purpose'], tooltipField: 'purpose', valueFormatter : this.utilService.purposeValueToString, width:120 },
+            { field: 'warningLevel', headerName : translations['Warning'], tooltipField: 'warningLevel', width:100 },
+            { field: 'soc', headerName : translations['soc(%)'], tooltipField: 'soc', width:100 },
+          ];
+          if (this.gridApi) {
+            this.gridApi.setColumnDefs(this.columnDefs);
+            this.gridApi.refreshHeader();
+          }
+          console.log("Table are translating", this.columnDefs);
+        });
+      });
+    });
+
+   }
+
+   //MINE//
+   isDropdownOpen = false;
+
+   toggleDropdown():void{
+     this.isDropdownOpen = !this.isDropdownOpen;
+   }
+ 
+  //  changeLanguage(language:string): void{
+  //    this.language = language;
+  //  } 
+ 
+  onLanguageChange(event: any) {
+   const language = event.target.value;
+   this.translate.use(language).subscribe(() => {
+     // Translation changed successfully
+     this.translateColumnHeaders();
+   });
+ }
+
+ /*translateColumnHeaders(): void {
+  this.columnDefs.forEach((columnDef) => {
+    columnDef.headerName = this.translate.instant(columnDef.headerName);
+  });
+}*/
 
   getPageSize(){
     this.gridHeight = this.realTimeMonitoringGrid.nativeElement.offsetHeight;
@@ -144,6 +233,9 @@ export class MonitoringComponent implements OnInit {
       this.gridApi.sizeColumnsToFit()
     }
   }
+  
+
+
 
   onBtExport() {
 
